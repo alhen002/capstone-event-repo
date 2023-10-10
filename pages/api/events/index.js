77467d434b48810367b48e0be99aac0c763f68f0
robin schubert <robschu123@hotmail.de>
2023-10-10 12:18:20 +0200

    added api folder and events endpoint

---
+++ pages/api/events/index.js
@@ -0,0 +1,19 @@
+import dbConnect from "@/db/dbConnect.js";
+import Event from "@/db/models/Events";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  switch (request.method) {
+    case "GET":
+      try {
+        const allEvents = await Event.find();
+        if (allEvents.length === 0 || !allEvents) {
+          throw new Error("Oops no event found!");
+        }
+        return response.status(200).json(allEvents);
+      } catch (error) {
+        return response.status(400).json({ message: error.message });
+      }
+  }
+}
