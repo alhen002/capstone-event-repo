11b7dabcad041312e3cc8d2604acadbf76e5b60a
robin schubert <robschu123@hotmail.de>
2023-10-19 15:41:22 +0200

    deleted some not nessesary code

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
-import useSWR from "swr";
 
 // * utils
 import fetcher from "@/lib/fetcher";
-import { getURL } from "@/lib/utils";
-import { groupByProperty } from "@/lib/utils";
-//* hooks
-import useFilters from "@/hooks/useFilters";
+
 //* components
 import Layout from "@/components/Layout";
 
