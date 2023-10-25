5958559c40bb62ed66a4d624521d3e3c84efc0e4
robin schubert <robschu123@hotmail.de>
2023-10-25 16:02:09 +0200

    edit backend, that just the owner id can edit events

--- pages/api/events/[id].js
+++ pages/api/events/[id].js
@@ -1,9 +1,14 @@
 import dbConnect from "@/db/dbConnect.js";
 import Event from "@/db/models/Events";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
+
+  const session = await getServerSession(request, response, authOptions);
+
   switch (request.method) {
     case "GET":
       try {
@@ -16,12 +21,23 @@ export default async function handler(request, response) {
         return response.status(500).json({ message: error.message });
       }
     case "PUT":
+      if (!session) {
+        return response
+          .status(401)
+          .json({ message: "You've must be logged in to edit an event." });
+      }
       try {
         const event = await Event.findByIdAndUpdate(id, request.body);
+
         if (!event) {
           return response
             .status(404)
             .json({ message: "Event not found. Could not be updated." });
+        }
+        if (session.id != event.organizer.toString()) {
+          return response
+            .status(401)
+            .json({ message: "You're not allowed to edit this event." });
         }
         return response
           .status(200)
@@ -29,7 +45,6 @@ export default async function handler(request, response) {
       } catch (error) {
         return response.status(500).json({ message: error.message });
       }
-
     default:
       return response.status(405).json({ message: "Method not allowed" });
 
