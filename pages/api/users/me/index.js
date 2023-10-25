c53b10654737a9b3981c1da644e8379f6df95f1a
robin schubert <robschu123@hotmail.de>
2023-10-25 10:22:34 +0200

    edit the backend for get events and user

---
+++ pages/api/users/me/index.js
@@ -0,0 +1,28 @@
+// hier werden alle informationen des nutzers zurückgegeben
+import dbConnect from "@/db/dbConnect";
+import { authOptions } from "../../auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import User from "@/db/models/Users";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  const session = await getServerSession(request, response, authOptions);
+  if (!session) {
+    return response
+      .status(401)
+      .json({ message: "You must be logged in to access this route." });
+  }
+
+  switch (request.method) {
+    case "GET":
+      const user = await User.findById(session.id);
+      if (!user) {
+        return response.status(404).json({ message: "User not found" });
+      }
+      return response.status(200).json(user);
+
+    default:
+      return response.status(405).json({ message: "Method not allowed" });
+  }
+}
