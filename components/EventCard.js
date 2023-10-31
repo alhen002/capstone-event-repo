6c02bcba6e58680dcaa2f0bf4d1fea7a24d78fe6
Mukaddes Uzdoganlar <m_uzdoganlar@web.de>
2023-10-31 16:29:58 +0100

    cleaned up EventCard Component

--- components/EventCard.js
+++ components/EventCard.js
@@ -8,44 +8,7 @@ import { useSession } from "next-auth/react";
 import useSWR from "swr";
 import { useState } from "react";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
-import { PlusCircleIcon } from "@heroicons/react/24/outline";
-import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import { StarIcon } from "@heroicons/react/24/outline";
-
-const StyledLink = styled(Link)`
-  position: relative;
-  padding-inline: 1rem;
-  padding-block: 2rem;
-  border: 1px solid #000000;
-  min-height: 9rem;
-  border-radius: 5px;
-`;
-const StyledImage = styled(Image)`
-  z-index: -1;
-  object-fit: cover;
-`;
-
-const StyledTitle = styled.h2`
-  color: #ffffff;
-  font-size: 1rem;
-  position: absolute;
-  left: 1rem;
-  bottom: 0.5rem;
-`;
-
-const StyledCity = styled.span`
-  font-size: 0.75rem;
-`;
-
-const StyledDate = styled.p`
-  color: #ffffff;
-  font-size: 0.75rem;
-  position: absolute;
-  right: 1rem;
-  bottom: 0.5rem;
-`;
-
-// NEW CARD
 
 const StyledEventCard = styled.div`
   align-items: flex-start;
@@ -115,7 +78,7 @@ const StyledChevronRight = styled(ChevronRightIcon)`
   bottom: 16px;
 `;
 
-const StyledPlusCircle = styled(StarIcon)`
+const StyledStarIcon = styled(StarIcon)`
   height: 40px;
   width: 40px;
   ${(props) => {
@@ -134,19 +97,6 @@ const StyledPlusCircle = styled(StarIcon)`
   z-index: 2;
 `;
 
-const StyledCheckCircle = styled(CheckCircleIcon)`
-  height: 44px;
-  width: 44px;
-  position: absolute;
-  fill: var(--primary);
-  top: 12px;
-  right: 12px;
-  transition: 1s;
-  z-index: 2;
-  backdrop-filter: blur(10px);
-  border-radius: 50%;
-`;
-
 export default function EventCard({ event = {}, mutate }) {
   const { day, month } = getDate(event.startDateTime);
   const { data: session } = useSession();
@@ -161,38 +111,26 @@ export default function EventCard({ event = {}, mutate }) {
   }
 
   return (
-    <>
-      <StyledLink href={`/events/${event._id}`}>
-        <StyledImage
+    <StyledEventCard>
+      <StyledEventCardImageContainer>
+        {session?.id && (
+          <button onClick={handleToggleAttending}>
+            {isAttending ? (
+              <StyledStarIcon />
+            ) : (
+              <StyledStarIcon $variant={"filled"} />
+            )}
+          </button>
+        )}
+
+        <StyledEventCardImage
           src={event.imageUrl}
           alt={event.title.toLowerCase()}
-          fill={true}
           quality={50}
+          fill={true}
         />
-        <StyledTitle>
-          {event.title}, <StyledCity>{event.city}</StyledCity>
-        </StyledTitle>
-        <StyledDate>{`${day}. ${month}`}</StyledDate>
-      </StyledLink>{" "}
-      {session?.id && (
-        <Button variant={"secondary"} onClick={handleToggleAttending}>
-          {isAttending ? "Won't attend" : "Attend"}
-        </Button>
-      )}
-      <StyledEventCard>
-        <StyledEventCardImageContainer>
-          {isAttending ? (
-            <StyledPlusCircle />
-          ) : (
-            <StyledPlusCircle $variant={"filled"} />
-          )}
-          <StyledEventCardImage
-            src={event.imageUrl}
-            alt={event.title.toLowerCase()}
-            quality={50}
-            fill={true}
-          />
-        </StyledEventCardImageContainer>
+      </StyledEventCardImageContainer>
+      <Link href={`/events/${event._id}`}>
         <StyledEventCardTextContainer>
           <StyledInfoContainer>
             <StyledEventCardCity>{event.city}</StyledEventCardCity>
@@ -201,7 +139,7 @@ export default function EventCard({ event = {}, mutate }) {
           <StyledEventCardTitle>{event.title}</StyledEventCardTitle>
           <StyledChevronRight />
         </StyledEventCardTextContainer>
-      </StyledEventCard>
-    </>
+      </Link>
+    </StyledEventCard>
   );
 }
