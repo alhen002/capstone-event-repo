44e3325dc47be65d658bee685401a4c6e9a7c4ef
robin schubert <robschu123@hotmail.de>
2023-10-31 16:40:33 +0100

    add more styling

--- components/Form/Select.js
+++ components/Form/Select.js
@@ -8,9 +8,22 @@ const StyledContainer = styled.div`
   width: 100%;
   max-width: 36rem;
 `;
-
-const StyledLabel = styled.label``;
-const StyledSelect = styled.select``;
+const StyledLabel = styled.label`
+  color: var(--text-accent);
+  font-size: 1rem;
+  font-family: inherit;
+  font-style: normal;
+  font-weight: 500;
+`;
+const StyledSelect = styled.select`
+  padding: 0.75rem;
+  font-size: 1rem;
+  border-radius: 0.75rem;
+  color: var(--primary);
+  border: 1px solid var(--primary);
+  background-color: var(--background);
+  width: 100%;
+`;
 
 export default function Select({ title, required, options }) {
   return (
@@ -21,23 +34,23 @@ export default function Select({ title, required, options }) {
       >
         {title}
         <span>{required && `*`}</span>
-        <StyledSelect
-          id={title.toLowerCase()}
-          name={title.toLowerCase()}
-          aria-labelledby={`${title.toLowerCase()}_label`}
-          required={required}
-        >
-          <option value="">Please pick a {title.toLowerCase()}</option>
-          {options.map((option, index) => (
-            <option
-              key={index}
-              value={option.toLowerCase().trim().replaceAll(" ", "")}
-            >
-              {option}
-            </option>
-          ))}
-        </StyledSelect>
       </StyledLabel>
+      <StyledSelect
+        id={title.toLowerCase()}
+        name={title.toLowerCase()}
+        aria-labelledby={`${title.toLowerCase()}_label`}
+        required={required}
+      >
+        <option value="">Please pick a {title.toLowerCase()}</option>
+        {options.map((option, index) => (
+          <option
+            key={index}
+            value={option.toLowerCase().trim().replaceAll(" ", "")}
+          >
+            {option}
+          </option>
+        ))}
+      </StyledSelect>
     </StyledContainer>
   );
 }
